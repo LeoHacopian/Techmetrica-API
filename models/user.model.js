01905e9dde0522edac9ec217d3d0797a11b44553
liaYaghobi <liayaghobi@gmail.com>
2023-05-08 02:21:41 -0700

    adds user table

---
+++ models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+  },
+ 
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const { mongoose, db } = require("./database")
 const Questionnaire = require("./routes/questionnaire.route")
 const FormResponse = require("./routes/FormResponse.route")
 const GameResult = require("./routes/result.router.js")    
+const User = require("./routes/user.route")
 
 app.use(cors())
 app.use(express.json())
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use('/question', Questionnaire)
 app.use('/form', FormResponse)
 app.use('/gameResult', GameResult)
+app.use('/user', User)
 
 app.get("/", (req, res) => {
     res.send("Hello World")
